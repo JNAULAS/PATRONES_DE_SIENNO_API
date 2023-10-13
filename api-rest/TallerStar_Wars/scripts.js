fa1d356c809bbdeb080d74642ee7afdd03126fe4
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-13 08:35:26 -0500

    Update

--- api-rest/TallerStar_Wars/scripts.js
+++ api-rest/TallerStar_Wars/scripts.js
@@ -1,57 +1,12 @@
 const API = 'https://swapi.dev/api/people/'
-let contador = 0
-const listHeader = ['Código', 'Nombre', 'asa']
-const data = [
-    ['John', 30, 'USA'],
-    ['Alice', 25, 'Canada'],
-    ['Bob', 28, 'UK'],
-];
 
 // 3. Retorna parte visual
 function pintaPersonaje(id, list) {
-
-    //let div = document.createElement('div')
-    // Creamos elemento tabla
-    const table = document.createElement('table');
-
-    // Recorremos la lista y agregamos a la tabla
-    const tableRows = data.map(rowData => {
-        // Agregamos fila
-        const row = document.createElement('tr');
-        if (id == 0) {
-            const cells = listHeader.map(cellColumm => {
-                const cell = document.createElement('td');
-                cell.textContent = cellColumm;
-                return cell;
-            });
-
-            // Agregamos celdas a las filas
-            cells.forEach(cell => {
-                row.appendChild(cell);
-            });
-        } else {
-            const cells = rowData.map(cellData => {
-                const cell = document.createElement('td');
-                cell.textContent = cellData;
-                return cell;
-            });
-
-            // Agregamos celdas a las filas
-            cells.forEach(cell => {
-                row.appendChild(cell);
-            });
-        }
-
-        return row;
-    });
-
-    // Agregamos las filas a la tabla
-    tableRows.forEach(row => {
-        table.appendChild(row);
-    });
-
-    // Agregamos tabla al dom
-    document.body.appendChild(table);
+    let div = document.createElement('div')
+    let h1_id = document.createTextNode('Codigo: '+ id + ' Nombre: ' + list.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_id)
+    div.appendChild(h1)
 
     // Pintado
     let contenedor = document.getElementById('contenedor')
@@ -62,7 +17,7 @@ function pintaPersonaje(id, list) {
 async function obtener_personaje(id) {
     try {
         let response = await fetch(`${API}${id}`)
-        return pintaPersonaje(id, await response.json())
+        return pintaPersonaje(id,await response.json())
     } catch (error) {
         console.error(`[error]: ${error}`)
     }
