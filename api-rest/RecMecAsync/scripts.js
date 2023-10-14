404629c9c0ba3466fe8ab3288f42148234087f63
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-13 23:13:21 -0500

    Se modifica creacion de vista

--- api-rest/RecMecAsync/scripts.js
+++ api-rest/RecMecAsync/scripts.js
@@ -1,23 +1,33 @@
 // Funcion para crear la vista
 function createView() {
-
     // Creamos componentes visuales
     let div = document.createElement('div')
     let h1 = document.createElement('h1')
-    let input = document.createElement('INPUT')
+    let inputCedula = document.createElement('INPUT')
+    let inputNombre = document.createElement('INPUT')
+    let labelCedula = document.createElement('label');
+    let labelNombre = document.createElement('label');
 
     // Creamos el conponente a ser  ingresado dentro del div
-    let textH1 = document.createTextNode('Prueba')
+    let textH1 = document.createTextNode('FORMULARIO PERSONAS')
     h1.appendChild(textH1)
-
-    input.setAttribute('type', 'text')
+    inputCedula.setAttribute('type', 'text')
+    inputNombre.setAttribute('type', 'text')
+    // Agregamos datos del label
+    labelCedula.textContent = 'Cedula: ';
+    labelNombre.textContent = 'Nombre: ';
     //Agregamos componentes creados al div
     div.appendChild(h1)
-    div.appendChild(input)
+
+    div.appendChild(labelCedula)
+    div.appendChild(inputCedula)
+
+    div.appendChild(labelNombre)
+    div.appendChild(inputNombre)
     // Agregamos componentes al root en este caso el componente principal
     //Creamos variable del componente roor para agregar los componentes visuales
     let contenedor = document.getElementById('contenedor')
-    contenedor.appendChild( div )
+    contenedor.appendChild(div)
 
 }
 //  Funcion para llamar a la vista
